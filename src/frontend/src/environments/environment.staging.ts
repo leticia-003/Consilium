071be0225875a0dff4bf255ec0fb3ddf0107c2cc
JoseCostaUni <up202207871@up.pt>
2025-11-06 01:45:09 +0000

    added different environments to the frontend

---
+++ src/frontend/src/environments/environment.staging.ts
@@ -0,0 +1,6 @@
+// src/frontend/src/environments/environment.staging.ts
+
+export const environment = {
+  production: true, // Keep this true for a production-like build
+  apiBaseUrl: 'https://consilium-api-devlopment.onrender.com/api' // Your staging API
+};
