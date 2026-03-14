dca1a2dc33401fa5f584a53fef9b8c8ce9d9d1a9
Letícia Coelho <leticiamendes2122@gmail.com>
2026-03-14 14:08:43 +0000

    updating url

--- src/frontend/src/environments/environment.prod.ts
+++ src/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // Production environment
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://consilium-api-prod-0v78.onrender.com/api' // Your production API on Render
+  apiBaseUrl: 'https://consilium-3q0f.onrender.com/api' // Your production API on Render
 };
